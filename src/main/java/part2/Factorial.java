2a921056ac4f1727d549702de6f584ccd5917432
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-11-24 18:35:15 +0100

    Some comments for factorial task

--- src/main/java/part2/Factorial.java
+++ src/main/java/part2/Factorial.java
@@ -4,6 +4,9 @@ import java.math.BigInteger;
 
 public class Factorial {
 
+    /*Реализуйте метод, вычисляющий факториал заданного натурального числа.
+      Поскольку это очень быстро растущая функция, то даже для небольших N вместимости типов int и long
+      очень скоро не хватит. Поэтому будем использовать BigInteger.*/
     public static BigInteger factorial(int value) {
         if (value==0) return BigInteger.valueOf(1);
         return ((value > 1) ? factorial(value - 1).multiply(BigInteger.valueOf(value)) : BigInteger.valueOf(1));
