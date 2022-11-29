1e5f7f6d6d12d0552a7ee1f20a7dfd9e41145ae0
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-11-29 10:59:07 +0100

    Added complex numbers task

---
+++ src/test/java/part3/ComplexNumsTest.java
@@ -0,0 +1,21 @@
+package test.java.part3;
+
+import main.java.part3.complexnumbers.ComplexNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComplexNumsTest {
+
+    ComplexNumber a = new ComplexNumber(1, 1);
+    ComplexNumber b = new ComplexNumber(1, 1);
+
+    @Test
+    public void hashCodeTest() {
+        Assertions.assertEquals(a.hashCode(),b.hashCode());
+    }
+
+    @Test
+    public void equalsTest() {
+        Assertions.assertTrue(a.equals(b));
+    }
+}
