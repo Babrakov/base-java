4e104da69d431963b91e6ad6aa8964e9aa847ac5
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-12-29 18:00:26 +0100

    Added exception intro task

---
+++ src/main/java/ru/infoza/part4/ExceptionsIntro.java
@@ -0,0 +1,45 @@
+package ru.infoza.part4;
+
+public class ExceptionsIntro {
+//    public static void main(String[] args) {
+//        System.out.println(getCallerClassAndMethodName());
+//        method();
+////        anotherMethod();
+//
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        m1();    }
+
+    static void m1() {
+        System.out.println(getCallerClassAndMethodName());
+        m2();    }
+
+    static void m2() {
+        System.out.println(getCallerClassAndMethodName());
+        m3();    }
+
+    static void m3() {
+        System.out.println(getCallerClassAndMethodName());    }
+
+    private static void method() {
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        if(stackTraceElements.length > 3) {
+            StackTraceElement element = stackTraceElements[3];
+            String className = element.getClassName();
+            String methodName = element.getMethodName();
+            return className + "#" + methodName;
+        }
+        return null; // your implementation here
+    }
+
+}
